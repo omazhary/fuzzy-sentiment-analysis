ab377e75574579bea20f3ad120a663d5a9d1dc16
omazhary <omazhary@gmail.com>
2016-04-27 22:35:32 +0200

    Split fixes and extensions
    * Expanded test case.
    * Removed old "split_on_space" reference.

--- main.rb
+++ main.rb
@@ -4,9 +4,7 @@ require_relative 'manipulation_syntax/clause_extractor'
 
 clause_extractor = ClauseExtractor.new("This is the first sentence. And this is the second one.")
 
-clause_extractor.split_on_space
-
-clause_extractor.src_text = "sentences, much more than sentences. SENTENCES! Give me more?"
+clause_extractor.src_text = "sentences, much more than sentences. SENTENCES! Give me more? And he said \"Ta-Da\""
 clause_extractor.split_clauses
 
 clause_extractor.clause_list.each do |clause|
