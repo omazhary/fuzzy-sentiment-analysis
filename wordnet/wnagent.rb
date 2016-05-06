29351d76c2ff1fa121502465ea6e3923d1901573
omazhary <omazhary@gmail.com>
2016-05-06 12:51:47 +0200

    Finished 'word-type' queries
    * Implemented the get_word_type method
    * Added logging capabilities to entire module

--- wordnet/wnagent.rb
+++ wordnet/wnagent.rb
@@ -5,6 +5,8 @@ module WORDNET
 require 'net/http'
 require 'uri'
 require 'nokogiri'
+require 'logger'
+require 'logger/colors'
 
     class WordNetAgent
 
@@ -13,33 +15,49 @@ require 'nokogiri'
         def initialize()
             @@wn_word_uri = "http://wordnetweb.princeton.edu/perl/webwn?s="
             @response = ""
+            @logger = Logger.new(STDERR)
         end
 
         # Sends word queries to wordnet's web interface.
         def send_query(query = "")
-            query_full = @@wn_word_uri + query
+            query_full = @@wn_word_uri + query + "&o0=&o1="
             query_uri = URI.parse(query_full)
             query_response = nil
-        #    begin
+            begin
                 query_http = Net::HTTP.new(query_uri.host, query_uri.port)
                 query_request = Net::HTTP::Get.new(query_uri.request_uri)
                 query_response = query_http.request(query_request)
-                puts query_response.inspect
-        #    rescue
-        #        puts "An exception occured with the HTTP request \"#{@query_uri}\"\n which returned a code #{query_response.code}"
-        #    end
-            #if query_response.code != 200
-        #        puts "An error occured with the HTTP request \"#{@query_uri}\"\n which returned a code #{query_response.code}"
-            #end
-            #@response = Nokogiri::XML(response.body)
+            rescue
+                @logger.fatal("An exception occured with the HTTP request \"#{@query_full}\"\n which returned a code #{query_response.code}")
+            end
+            if query_response.code != "200"
+                @logger.error("An error occured with the HTTP request \"#{@query_full}\"\n which returned a code #{query_response.code}")
+            else
+                @response = Nokogiri::XML(query_response.body)
+                @response.errors.each do |error|
+                    @logger.warn(error)
+                end
+            end
         end
 
         # Gets the type(s) of a word by querying wordnet
         def get_word_type(word = "")
             word_type = []
             send_query(word)
-            type = @response.xpath('//h3').content
-            puts type.inspect
+            types = @response.css("h3")
+            types.each do |type|
+                if type.text == "Your search did not return any results."
+                    @logger.error("WordNet was unable to recognize the word #{word}")
+                else
+                    word_type.push(type.text)
+                end
+            end
+            if types.length == 0
+                @logger.error("Unable to retrieve type for word #{word}.")
+            elsif types.length > 1
+                @logger.info("Word #{word} has more than one type depending on the context.")
+            end
+            return word_type
         end
 
     end
