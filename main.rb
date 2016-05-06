4a7e887d84f33bbdf4f8c65276e60ee5501fce78
omazhary <omazhary@gmail.com>
2016-05-06 22:46:56 +0200

    Tested out the new "Text" component

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 # This is the main file that will run this program
 
-require_relative 'nlp/paragraph'
+require_relative 'nlp/text'
 require_relative 'wordnet/wnagent'
 
 test_text1 = "This is the first sentence. And this is the second one."
@@ -9,5 +9,5 @@ test_text2 = "sentences, much more than sentences. SENTENCES! Give me more? And 
 
 wn_agent = WORDNET::WordNetAgent.new
 
-para1 = NLP::Paragraph.new(test_text1, wn_agent)
-para2 = NLP::Paragraph.new(test_text2, wn_agent)
+para1 = NLP::Text.new(test_text1, wn_agent)
+para2 = NLP::Text.new(test_text2, wn_agent)
