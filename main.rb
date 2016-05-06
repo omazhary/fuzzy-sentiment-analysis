65d222a0e868eba9c878248fd3ea167ccfef5673
omazhary <omazhary@gmail.com>
2016-05-06 12:55:11 +0200

    Tested out WordNetClient.

--- main.rb
+++ main.rb
@@ -7,15 +7,7 @@ test_text1 = "This is the first sentence. And this is the second one."
 
 test_text2 = "sentences, much more than sentences. SENTENCES! Give me more? And he said \"Ta-Da\""
 
-para1 = NLP::Paragraph.new(test_text1)
-para2 = NLP::Paragraph.new(test_text2)
-
 wn_agent = WORDNET::WordNetAgent.new
 
-para2.sentence_list.each do |sentence|
-    sentence.clause_list.each do |clause|
-        clause.word_list.each do |word|
-            wn_agent.send_query(word.src_text)
-        end
-    end
-end
+para1 = NLP::Paragraph.new(test_text1, wn_agent)
+para2 = NLP::Paragraph.new(test_text2, wn_agent)
