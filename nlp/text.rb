2ce41ec1e5a0fbe00eee81cf4d99a47c8ffd264c
omazhary <omazhary@gmail.com>
2016-05-06 22:46:18 +0200

    Added a "Text" component for multi-paragraph corpora

---
+++ nlp/text.rb
@@ -0,0 +1,29 @@
+# This class represents texts that consist of multiple paragraph.
+
+require_relative 'paragraph'
+
+module NLP
+
+    class Text
+        attr_accessor :src_text
+        attr_reader :paragraph_list
+
+        def initialize(text = "", wn_agent = WORDNET::WordNetAgent.new)
+            @src_text = text
+            @paragraph_list = []
+            @wn_agent = wn_agent
+            self.split_paragraphs
+        end
+
+        def split_paragraphs
+            # This is merely an assumption that paragraphs can be delimited by newlines.
+            # I will need to check the validity of this assumption when checking out corpora.
+            paragraphs_raw = @src_text.split(/\n/)
+            paragraphs_raw.each do |paragraph|
+                paragraph = paragraph.strip
+                @paragraph_list.push(NLP::Paragraph.new(paragraph, @wn_agent))
+            end
+        end
+    end
+
+end
