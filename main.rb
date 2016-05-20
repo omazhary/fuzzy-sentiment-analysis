45565a7f5e36ac958e0050e00ec6ef4dfe103423
Omar Elazhary <omar.elazhary@sap.com>
2016-05-20 15:40:55 +0200

    Tested out the refactored changes.

--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 # This is the main file that will run this program
 
-require_relative 'textmining/sentalysis/uncertain_trainer'
+require_relative 'textmining/sentalysis/fuzzy/fuzzy_trainer'
 
-trainer = SENTALYSIS::UncertainTrainer.new("polarity_index.json")
+index = SENTALYSIS::PolarityIndex.new("sample_index.json")
+
+trainer = SENTALYSIS::FuzzyTrainer.new(index, "positive", "sample_Dir")
 trainer.train("bla", "negative")
