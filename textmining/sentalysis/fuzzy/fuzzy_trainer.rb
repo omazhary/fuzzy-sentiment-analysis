4effaf5926ff017acdc6d18e8c7610b21d4bec7c
omazhary <omazhary@gmail.com>
2016-05-21 23:43:02 +0200

    Started to add training logic

--- textmining/sentalysis/fuzzy/fuzzy_trainer.rb
+++ textmining/sentalysis/fuzzy/fuzzy_trainer.rb
@@ -3,30 +3,55 @@
 require 'logger'
 require 'logger/colors'
 require_relative 'polarity_index'
+require_relative '../../../nlp/text'
+require_relative '../../../wordnet/wnagent'
 
 module SENTALYSIS
 
     class FuzzyTrainer
     
         attr_accessor :polarity
-        attr_accessor :corpora_dir
+        attr_accessor :training_dir
         attr_accessor :polarity_index
+        attr_reader :wordnet_agent
         
-        def initialize(index = SENTALYSIS::PolarityIndex.new('sample_index.json'), current_polarity = 'positive', training_dir)
+        def initialize(index = SENTALYSIS::PolarityIndex.new('sample_index.json'), current_polarity = 'positive', wn_agent = WORDNET::WordNetAgent.new, training_dir)
             @logger = Logger.new(STDERR)
-            if training_dir == nil
-                @logger.fatal("No training directory provided. The training process cannot proceed.")
+            if training_dir == nil || training_dir.empty? || !File.directory?(training_dir) || Dir[training_dir + "/*"].empty?
+                @logger.fatal("No valid training directory provided. The training process cannot proceed.")
                 abort("Aborting due to error.")
             else
                @training_dir = training_dir 
             end
             @polarity_index = index
             @polarity = current_polarity
+            @wordnet_agent = wn_agent
         end
         
-        def train(corpora_directory = "<empty>", polarity = "positive")
-            @polarity_index.update_polarity("test", "negative")
-            @polarity_index.save_polarity_file
+        def train
+            files = Dir.entries(@training_dir)
+            files.each do |filename|
+                full_filename = File.absolute_path("#{training_dir}/#{filename}")
+                if File.exists?(full_filename) && File.file?(full_filename) && File.readable?(full_filename)
+                    # Open the file and read its contents:
+                    file = File.new(full_filename, "r")
+                    src_text = file.read
+                    file.close
+                    # Parse the file using the NLP elements:
+                    text = NLP::Text.new(src_text, @wordnet_agent)
+                    # Filter out adjectives:
+                    text.paragraph_list.each do |paragraph|
+                        paragraph.sentence_list.each do |sentence|
+                            sentence.clause_list.each do |clause|
+                                clause.word_list.each do |word|
+                                    puts word.types.inspect
+                                end
+                            end
+                        end
+                    end
+                    # Update adjective polarity:
+                end
+            end
         end        
     end
     
