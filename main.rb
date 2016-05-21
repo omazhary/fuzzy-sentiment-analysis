bf277339432d691fd34373ff74146e4bcd4a7faa
omazhary <omazhary@gmail.com>
2016-05-21 23:43:38 +0200

    Testing out new training logic

--- main.rb
+++ main.rb
@@ -1,8 +1,9 @@
 # This is the main file that will run this program
 
 require_relative 'textmining/sentalysis/fuzzy/fuzzy_trainer'
+require_relative 'wordnet/wnagent'
 
 index = SENTALYSIS::PolarityIndex.new("sample_index.json")
 
-trainer = SENTALYSIS::FuzzyTrainer.new(index, "positive", "sample_Dir")
-trainer.train("bla", "negative")
+trainer = SENTALYSIS::FuzzyTrainer.new(index, "positive", WORDNET::WordNetAgent.new, "../poldata/txt_sentoken/pos")
+trainer.train
