1fca77d1131e40090f856abceee5567dd949bc77
omazhary <omazhary@gmail.com>
2016-06-07 23:11:08 +0200

    Tested out statistical classifier

--- main.rb
+++ main.rb
@@ -1,10 +1,20 @@
+#!/usr/bin/env ruby
+
 # This is the main file that will run this program
 
 require_relative 'textmining/sentalysis/fuzzy/fuzzy_trainer'
+require_relative 'textmining/sentalysis/fuzzy/fuzzy'
 require_relative 'wordnet/wnagent'
 require 'logger'
 
 index = SENTALYSIS::PolarityIndex.new("sample_index.json")
+wnagent = WORDNET::WordNetAgent.new(Logger::INFO)
 
-trainer = SENTALYSIS::FuzzyTrainer.new(index, "positive", WORDNET::WordNetAgent.new(Logger::ERROR), "../poldata/txt_sentoken/pos")
-trainer.train
+classifier = SENTALYSIS::FuzzyClassifier.new(index, wnagent)
+
+test_text = NLP::Text.new("This tastes awful. But that is amazing!", wnagent)
+
+puts classifier.classify(test_text)
+
+#trainer = SENTALYSIS::FuzzyTrainer.new(index, "negative", wnagent, "../poldata/txt_sentoken/neg")
+#trainer.train
