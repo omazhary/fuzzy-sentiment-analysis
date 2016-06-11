453bfa263c48821912ac367213f1c316b1ad386a
omazhary <omazhary@gmail.com>
2016-06-12 00:42:40 +0200

    Using the test harness

--- main.rb
+++ main.rb
@@ -2,19 +2,17 @@
 
 # This is the main file that will run this program
 
+require_relative 'textmining/sentalysis/fuzzy/fuzzy_tester'
 require_relative 'textmining/sentalysis/fuzzy/fuzzy_trainer'
 require_relative 'textmining/sentalysis/fuzzy/fuzzy'
 require_relative 'wordnet/wnagent'
 require 'logger'
 
 index = SENTALYSIS::PolarityIndex.new("sample_index.json")
-wnagent = WORDNET::WordNetAgent.new(Logger::INFO)
+wnagent = WORDNET::WordNetAgent.new(Logger::ERROR)
 
-classifier = SENTALYSIS::FuzzyClassifier.new(index, wnagent)
-
-test_text = NLP::Text.new("This tastes awful. But that is amazing!", wnagent)
-
-puts classifier.classify(test_text)
+tester = SENTALYSIS::FuzzyTester.new(index, "positive", wnagent, "../poldata/txt_sentoken/pos")
+tester.test
 
 #trainer = SENTALYSIS::FuzzyTrainer.new(index, "negative", wnagent, "../poldata/txt_sentoken/neg")
 #trainer.train
